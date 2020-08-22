f84a5ee3d7994b3564faa3d4238f77de853f6870
marcioho <marcioho@gmail.com>
2020-08-22 17:18:54 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -15,11 +15,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
 
-//} else {
-urlDB = 'mongodb+srv://marcioho:An@jul1a@cluster0.7ojww.mongodb.net/cafe';
-//}
+} else {
+    urlDB = 'mongodb+srv://marcioho:An@jul1a@cluster0.7ojww.mongodb.net/cafe';
+}
 
 process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser')
 const express = require('express');
 const mongoose = require('mongoose');
 
-
-
 const app = express();
 
 // parse application/x-www-form-urlencoded
